404900f999ba2bedbced1aa67c2354ab9358f4ee
Fabiano Leite <fabiano.marechal@gmail.com>
2024-04-16 00:56:36 -0300

    calcula saldo de vitorias

--- index.js
+++ index.js
@@ -1,24 +1,54 @@
-let nomeDoHeroi = "Hercules";
-let experiencia = 10001;
+let dadosDoHeroi = {
+  nome: "Thor",
+  experiencia: 10001,
+  vitorias: 85,
+  derrotas: 20
+};
 
 let nivel;
 
-if (experiencia <= 1000) {
+if (dadosDoHeroi.experiencia <= 1000) {
     nivel = "Ferro";
-} else if (experiencia >= 1001 && experiencia <= 2000) {
+} else if (dadosDoHeroi.experiencia >= 1001 && dadosDoHeroi.experiencia <= 2000) {
     nivel = "Bronze";
-} else if (experiencia >= 2001 && experiencia <= 5000) {
+} else if (dadosDoHeroi.experiencia >= 2001 && dadosDoHeroi.experiencia <= 5000) {
     nivel = "Prata";
-} else if (experiencia >= 5001 && experiencia <= 7000) {
+} else if (dadosDoHeroi.experiencia >= 5001 && dadosDoHeroi.experiencia <= 7000) {
     nivel = "Ouro";
-} else if (experiencia >= 7001 && experiencia <= 8000) {
+} else if (dadosDoHeroi.experiencia >= 7001 && dadosDoHeroi.experiencia <= 8000) {
     nivel = "Platina";
-} else if (experiencia >= 8001 && experiencia <= 9000) {
+} else if (dadosDoHeroi.experiencia >= 8001 && dadosDoHeroi.experiencia <= 9000) {
     nivel = "Ascendente";
-} else if (experiencia >= 9001 && experiencia <= 10000) {
+} else if (dadosDoHeroi.experiencia >= 9001 && dadosDoHeroi.experiencia <= 10000) {
     nivel = "Imortal";
-} else if (experiencia >= 10001) {
+} else if (dadosDoHeroi.experiencia >= 10001) {
     nivel = "Radiante";
 }
 
-console.log(`O Herói de nome ${nomeDoHeroi} está no nível de ${nivel}`);
+console.log(`O Herói de nome ${dadosDoHeroi.nome} está no nível ${nivel}`);
+
+function calcularNivel(vitorias, derrotas) {
+  let saldoVitorias = vitorias - derrotas;
+  let nivel;
+
+  if (vitorias < 10) {
+      nivel = "Ferro";
+  } else if (vitorias >= 10 && vitorias <= 20) {
+      nivel = "Bronze";
+  } else if (vitorias >= 21 && vitorias <= 50) {
+      nivel = "Prata";
+  } else if (vitorias >= 51 && vitorias <= 80) {
+      nivel = "Ouro";
+  } else if (vitorias >= 81 && vitorias <= 90) {
+      nivel = "Diamante";
+  } else if (vitorias >= 91 && vitorias <= 100) {
+      nivel = "Lendário";
+  } else if (vitorias >= 101) {
+      nivel = "Imortal";
+  }
+
+  return `O Herói tem saldo de ${saldoVitorias} vitórias e está no nível ${nivel}`;
+}
+
+let resultado = calcularNivel(dadosDoHeroi.vitorias, dadosDoHeroi.derrotas);
+console.log(resultado);
